f66a71f6c8f12404ac71c9256e26b4ee3d6513cd
yun <chiaoyunlo@gmail.com>
2020-07-17 00:20:38 +0800

    照範例的刪除鍵版本

--- js/all.js
+++ js/all.js
@@ -1,1 +1,54 @@
+//DOM
 var sendBtn = document.querySelector('.send');
+// var deleteBtn = document.querySelector('.delete');
+var ulList = document.querySelector('.list');
+var data = JSON.parse(localStorage.getItem('todoText'))||[];
+
+//監聽"加入代辦"和"刪除"鈕
+sendBtn.addEventListener('click', addList, false);
+ulList.addEventListener('click', deleteList, false);
+showList(data);
+
+//加入陣列及local storage
+function addList(e) {    
+    e.preventDefault();
+    //將輸入的值加入陣列
+    var todoText = document.querySelector('.text').value;
+    // 不得輸入空白
+    if (todoText === ""){
+        return;
+    }
+    data.push(todoText); 
+    showList(data); 
+    //將data轉為字串
+    // var todoString = JSON.stringify(data);
+    //把字串存入localStorage
+    // localStorage.setItem('todoText', todoString);
+    localStorage.setItem('todoText', JSON.stringify(data)); 
+}
+
+//更新清單畫面
+function showList(){
+    str = "";
+    data.forEach((item,index) => {
+        str += `<li>
+                <button class="delete" data-index = ${index}><span class="material-icons">
+                delete_forever</span></button>${item}
+                </li>`;
+    });    
+    ulList.innerHTML = str;
+}
+
+//刪除項目
+function deleteList(e) {
+    e.preventDefault();
+    console.log(e.target);
+    if(e.target.nodeName !== 'BUTTON'){
+        return;
+    };
+    var index = e.target.dataset.index;
+    data.splice(index, 1);
+    localStorage.setItem('todoText', JSON.stringify(data));
+    // 重新渲染清單
+    showList(data);
+}
